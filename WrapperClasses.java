d2fe9ec005e602d9b78bd180271ea7b314f56d8d
SabaV <37605780+sabavs@users.noreply.github.com>
2020-01-30 21:00:08 -0500

    Update WrapperClasses.java

--- WrapperClasses.java
+++ WrapperClasses.java
@@ -5,11 +5,5 @@ public class WrapperClasses {
         Integer ii = new Integer(10); //putting a primitive value inside an object
         int j = ii.intValue();
         System.out.println(j);
-
-        Integer value = 15;
-        int k = value;
-
-        System.out.println(int k);
-
     }
 }
