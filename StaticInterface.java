9b5a88b21a2b81cc4ee328a194aca148058b84b4
SabaV <37605780+sabavs@users.noreply.github.com>
2020-02-03 16:23:50 -0500

    Create StaticInterface.java

---
+++ StaticInterface.java
@@ -0,0 +1,11 @@
+interface Demo{
+    void abc();
+    static void show(){
+        System.out.println("Hello");
+    }
+}
+public class Interface {
+    public static void main(String[] args){
+     Demo.show();
+    }
+}
