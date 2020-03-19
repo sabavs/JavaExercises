eadcdd796a2129a463a2ec33ba03e199dd6d6ba7
SabaV <37605780+sabavs@users.noreply.github.com>
2020-03-19 17:22:22 -0400

    Create isPalindrome.java

---
+++ isPalindrome.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class thirdReverse{
+
+        public static String reversed(String word){
+            String wordReversed = "";
+            for(int i = word.length()-1; i>=0;i--){
+                wordReversed = wordReversed + word.charAt(i);
+            }
+            return wordReversed;
+        }
+
+        public static void isPalindrome(String word) {
+            if (word.equalsIgnoreCase(reversed(word))) {
+                System.out.println(word + " is a palindrome.");
+            } else {
+                System.out.println(word + " is not a palindrome.");
+            }
+        }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word to be reversed: ");
+        String word = scanner.nextLine();
+        System.out.println("You entered " + word + ".");
+        reversed(word);
+        System.out.println("Your word reversed is: " + reversed(word) + ".");
+        isPalindrome(word);
+    }
+}
